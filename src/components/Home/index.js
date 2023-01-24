557d6f48e1ec254861706981230606570b726f9b
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2023-01-24 22:45:56 +0100

    Update Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,26 @@
-import LogoTitle from '../../assets/images/logo-w.png';
+
+import { Link } from 'react-router-dom'
+import './index.scss'
 
 const Home = () => {
-  return ( 
+  return (
     <div className="container home-page">
-        <div className="text-zone">
-            <h1>Hello,<br /> I'm
-            {/* <img src={LogoTitle} alt="developer"/> */}
-            Łukasz
-            <br />
-web developer </h1>
-        </div>
-
+      <div className="text-zone">
+        <h1>
+          Hello,
+          <br /> I'm
+          <span className='surname'>W</span>
+          Lukasz
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer / Javascript Junior</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
-);
+  )
 }
 
 export default Home
